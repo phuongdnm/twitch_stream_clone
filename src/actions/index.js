bea03cfbe383d303cbf09a4e846c9a1745027b80
Phuong Minh <phuongminh2303@gmail.com>
2019-08-11 20:48:19 +0700

    Create Streams through Action Creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import {SIGN_IN, SIGN_OUT} from './types';
+import streams from '../apis/streams';
 
 export const signIn = (userId) => {
   return {
@@ -12,3 +13,9 @@ export const signOut = () => {
     type: SIGN_OUT
   };
 };
+
+export const createStream = (formValues) => {
+  return async (dispatch) => {
+    streams.post('/streams', formValues);
+  }
+}
