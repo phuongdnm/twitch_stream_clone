3855cd01e67a0e07ec5ce58a13e10273d6c67184
Phuong Minh <phuongminh2303@gmail.com>
2019-08-15 21:35:20 +0700

    Add Action Creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
+// prettier-ignore
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, DELETE_STREAM, EDIT_STREAM } from './types';
 import streams from '../apis/streams';
 
 export const signIn = userId => {
@@ -18,6 +19,41 @@ export const createStream = formValues => {
   return async dispatch => {
     const response = await streams.post('/streams', formValues);
 
-    dispatch({type: CREATE_STREAM, payload: response.data})
+    dispatch({ type: CREATE_STREAM, payload: response.data });
   };
 };
+
+export const fetchStreams = () => {
+  return async dispatch => {
+    const response = await streams.get('/streams');
+
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
+  };
+};
+
+export const fetchStream = id => {
+  return async dispatch => {
+    const response = await streams.get(`$/streams/${id}`);
+
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+  };
+};
+
+export const editStream = (id, formValues) => {
+  return async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+  };
+};
+
+export const deleteStream = id => {
+  return async dispatch => {
+    await streams.delete(`/streams/${id}`);
+
+    dispatch({ type: DELETE_STREAM, payload: id });
+  };
+};
+
+
+// VIDEO 8
