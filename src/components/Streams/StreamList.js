ca7165b2f7dd9fc725c7eff36c72e95fdfda3c7b
Phuong Minh <phuongminh2303@gmail.com>
2019-08-17 22:49:48 +0700

    Add 'Create stream' button when user is logged in

--- src/components/Streams/StreamList.js
+++ src/components/Streams/StreamList.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
+import { Link } from 'react-router-dom';
 
 class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
 
-  renderAdmin = (stream) => {
+  renderAdmin = stream => {
     if (stream.userId === this.props.currentUserId) {
       return (
         <div className="right floated content">
-          <button className="ui button primary">
-            Edit
-          </button>
-          <button className="ui button negative">
-            Delete
-          </button>
+          <button className="ui button basic blue">Edit</button>
+          <button className="ui button basic red">Delete</button>
         </div>
-      )
+      );
     }
-  }
+  };
 
   renderList = () => {
     return this.props.streams.map(stream => {
@@ -37,11 +34,24 @@ class StreamList extends React.Component {
     });
   };
 
+  renderCreate = () => {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to="/streams/new" className="ui button basic green">
+            Create a stream
+          </Link>
+        </div>
+      );
+    }
+  };
+
   render() {
     return (
       <div>
         <h2>Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
       </div>
     );
   }
@@ -50,7 +60,8 @@ class StreamList extends React.Component {
 const mapStateToProps = state => {
   return {
     streams: Object.values(state.streams),
-    currentUserId: state.auth.userId
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
   };
 };
 
