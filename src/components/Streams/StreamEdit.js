8b30dfa5e65a007372d0e49eea1199f8d580a757
Phuong Minh <phuongminh2303@gmail.com>
2019-08-19 14:23:10 +0700

    Set up initial value for StreamForm through StreamEdit

--- src/components/Streams/StreamEdit.js
+++ src/components/Streams/StreamEdit.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
+import _ from 'lodash';
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = formValues => {
+    console.log(formValues);
+  };
+
   render() {
     const { stream } = this.props;
     if (!stream) {
       return <div>Loading</div>;
     }
-    return <div>{this.props.stream.title}</div>;
+    return (
+      <div>
+        <h3>Edit this Stream</h3>
+        <StreamForm
+          onSubmit={this.onSubmit}
+          // only take title and description from stream object
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+        />
+      </div>
+    );
   }
 }
 
@@ -22,5 +37,5 @@ const mapStateToProps = (state, ownProps) => {
 
 export default connect(
   mapStateToProps,
-  { fetchStream }
+  { fetchStream, editStream }
 )(StreamEdit);
