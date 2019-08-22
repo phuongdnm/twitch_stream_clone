7c86d168245a7c9b17a81cab85186a3086aa4613
Phuong Minh <phuongminh2303@gmail.com>
2019-08-22 21:32:24 +0700

    Refactor Modal to be reusable

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,13 +8,13 @@ const Modal = props => {
       onClick={() => history.push('/')}
       className="ui dimmer modals visible active"
     >
-      <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
-        <div className="header">Delete Stream</div>
-        <div className="content">Are you sure want to delete this stream?</div>
-        <div className="actions">
-          <button className="ui button basic blue">Delete</button>
-          <button className="ui button basic red">Cancel</button>
-        </div>
+      <div
+        onClick={e => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector('#modal')
--- src/components/Streams/StreamDelete.js
+++ src/components/Streams/StreamDelete.js
@@ -2,10 +2,17 @@ import React from 'react';
 import Modal from '../Modal';
 
 const StreamDelete = props => {
+  const actions = (
+    <React.Fragment>
+      <button className="ui button basic blue">Delete</button>
+      <button className="ui button basic red">Cancel</button>
+    </React.Fragment>
+  )
+
   return (
     <div>
       Stream Delete
-      <Modal />
+      <Modal title="Delete Stream" content="Are you sure want to delete this stream???" actions={actions}/>
     </div>
   );
 };
